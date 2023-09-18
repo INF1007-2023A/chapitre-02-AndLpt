b5af519d9392e81922197de6ea9905b9b5cfbe98
And Lpt <andrelepatient@gmail.com>
2023-09-18 21:30:46 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -4,7 +4,7 @@ def majuscule(mot):
     # TODO completer la fonction ici
     nouveaumot = ""
     for c in mot:
-        nouveaumdot += ch(ord(c) - 32)
+        nouveaumot += ch(ord(c) - 32)
         return nouveaumot
 
 
